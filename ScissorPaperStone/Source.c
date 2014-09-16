86f8e0bca7fba3026a4e9304196bc787c630c959
BreenIsALie <m.jonsson2@gmail.com>
2014-09-16 15:57:38 +0200

    Filled functions, added comments.  RNG not working yet, will fix soon

--- ScissorPaperStone/Source.c
+++ ScissorPaperStone/Source.c
@@ -12,32 +12,44 @@ Stone > Scissor*/
 int main(void)
 {
 	int PC, PCScore=0, CPUScore=0;		/*PC=Player Character, CPU = Computer Choice*/
-	srand(time(NULL));
-	int CPU = rand() % 4;
+	srand(time(0));
+	int CPU = rand("%d") %4;
 
 	printf("\nWelcome to Scissor, Paper, Stone. Pick your weapon:");
-	printf("\n1: Scissor\n2: Paper\n3: Stone\n4: Exit");
+	printf("\n1: Scissor\n2: Paper\n3: Stone\n4: Exit\n");
 	scanf_s("%d", &PC);
 
-	while (PCScore < 10 || CPUScore < 10 || PC==4);
+	while (PCScore < 10 && CPUScore < 10)
 	{
-	
-		if (PC == CPU)
+		if (PC == CPU)		/*If PC and CPU are matched*/
 		{
-			printf("\nMAD has struck again");			
+			printf("\nMAD has struck again\nPick a new weapon");	
+			printf("\n1: Scissor\n2: Paper\n3: Stone\n4: Exit\n");
+			scanf_s("%d", &PC);
 		}
 
-		else if (PC == 1 && CPU == 2 || PC == 2 && CPU == 3 || PC == 3 && CPU == 1)
+		else if (PC == 1 && CPU == 2 || PC == 2 && CPU == 3 || PC == 3 && CPU == 1) /*If PC has better then CPU*/
 		{
+			printf("Your score: %d", PCScore++);
+			printf("CPU score: %d", CPUScore);
 			printf("\nCONGRATULATIONS: The foe has been defeated");
+			printf("\n1: Scissor\n2: Paper\n3: Stone\n4: Exit\n");
+			scanf_s("%d", &PC);
 		}
-		else if (PC == 2 && CPU == 1 || PC == 3 && CPU == 2 || PC == 1 && CPU == 3)
+		else if (PC == 2 && CPU == 1 || PC == 3 && CPU == 2 || PC == 1 && CPU == 3) /*If CPU has better then PC*/
 		{
+			printf("Your score: %d", PCScore);
+			printf("CPU score: %d", CPUScore++);
 			printf("\nBITTER DEFEAT: You have been vanquished by the foe");
+			printf("\n1: Scissor\n2: Paper\n3: Stone\n4: Exit\n");
+			scanf_s("%d", &PC);
 		}
+		else if (PC == 4)	/*Exit condition*/
+		{
+			break;
+		}		
 	}
-
-	printf("\n1: Scissor\n2: Paper\n3: Stone\n4: Exit");
-	scanf_s("%d", &PC);
-
+	printf("\nPress any key to exit\n");
+	getchar();
+	return 0;
 }
