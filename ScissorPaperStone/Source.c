929ee23abe22f3aea6fc7c8a6a23284a6427c7f4
BreenIsALie <m.jonsson2@gmail.com>
2014-09-20 15:36:22 +0200

    Complete rework of the code to match requirements. Included changes: New code, Own functions, More comments

--- ScissorPaperStone/Source.c
+++ ScissorPaperStone/Source.c
@@ -8,48 +8,76 @@ Stone > Scissor*/
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(void)
 {
-	int PC, PCScore=0, CPUScore=0;		/*PC=Player Character, CPU = Computer Choice*/
-	srand(time(0));
-	int CPU = 1 + (rand("%d") % 4);
+	int Player=0, PlayerScore = 0, CPUscore = 0;	/*Declaration for player choice and score counters*/
+	srand(time(NULL));
+	int CPU = 1 + (rand("%d") % 4);				/*CPU randomizes choice and set it to random number between 1 - 3 */
+	int ChoiceInput(void);
+	int CPUScoreUp();
+	int PlayerScoreUp();
 
-	printf("\nWelcome to Scissor, Paper, Stone. Pick your weapon:");
-	printf("\n1: Scissor\n2: Paper\n3: Stone\n4: Exit\n");
-	scanf_s("%d", &PC);
+	ChoiceInput();
 
-	while (PCScore < 10 && CPUScore < 10)
+	while (CPUscore < 10 && PlayerScore < 10)	/*Run loop until ether player or computer reaches 10 points*/
 	{
-		if (PC == CPU)		/*If PC and CPU are matched*/
-		{
-			printf("\nMAD has struck again\nPick a new weapon");	
-			printf("\n1: Scissor\n2: Paper\n3: Stone\n4: Exit\n");
-			scanf_s("%d", &PC);
-		}
 
-		else if (PC == 1 && CPU == 2 || PC == 2 && CPU == 3 || PC == 3 && CPU == 1) /*If PC has better then CPU*/
+		if (Player == CPU)						/*If Player and CPU are matched*/
 		{
-			printf("Your score: %d", PCScore++);
-			printf("CPU score: %d", CPUScore);
+			printf("MAD has struck again and you are matched. Pick a new weapon:");
+			ChoiceInput();
+		}
+		else if (Player == 1 && CPU == 2 || Player == 2 && CPU == 3 || Player == 3 && CPU == 1) /*If player has better then the computer*/
+		{
+			PlayerScoreUp();
 			printf("\nCONGRATULATIONS: The foe has been defeated");
-			printf("\n1: Scissor\n2: Paper\n3: Stone\n4: Exit\n");
-			scanf_s("%d", &PC);
+			ChoiceInput();
 		}
-		else if (PC == 2 && CPU == 1 || PC == 3 && CPU == 2 || PC == 1 && CPU == 3) /*If CPU has better then PC*/
+		else if (Player == 2 && CPU == 1 || Player == 3 && CPU == 2 || Player == 1 && CPU == 3) /*If CPU has better then Player*/
 		{
-			printf("Your score: %d", PCScore);
-			printf("CPU score: %d", CPUScore++);
+			CPUScoreUp();
 			printf("\nBITTER DEFEAT: You have been vanquished by the foe");
-			printf("\n1: Scissor\n2: Paper\n3: Stone\n4: Exit\n");
-			scanf_s("%d", &PC);
+			ChoiceInput();
 		}
-		else if (PC == 4)	/*Exit condition*/
+		else if (Player == 4)		/*If player picks 4 to exit, Break loop and go to exit*/
 		{
 			break;
-		}		
+		}
+		else						/*If Player picks a number not in the list*/
+		{
+			printf("HOLD IT ! No house rules or improvised weapons. Pick one from the list");
+			ChoiceInput();
+		}
 	}
-	printf("\nPress any key to exit\n");
+
+	printf("\nGame has ended. You got", PlayerScore, "and the computer got", CPUscore);
+	printf("\n\nPress any key to exit");
 	getchar();
 	return 0;
+}	
+
+int ChoiceInput(void)	/*Function to present user with choice prompt*/
+{
+	int Player;
+	printf("\nPick your weapon\n1: Scissor\n2: Paper\n3: Stone\n4: Exit\n");
+	scanf_s("%d", &Player);
+	return 0;
 }
+
+int PlayerScoreUp(void)
+{
+	int PlayerScore;
+	PlayerScore++;
+	return 0;
+}
+
+int CPUscoreUp(void)
+{
+	int CPUscore;
+	CPUscore++;
+	return 0;
+}
+
+
